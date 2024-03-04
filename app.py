3c0472a90c0a9b0689d12a25b45183f5e08404e2
OOlajide <70548782+OOlajide@users.noreply.github.com>
2024-03-04 08:39:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,7 @@ st.title("Arbitrum Grants Chatbot")
 
 with st.expander("About"):
     st.write("Chat with a dataset of all Arbitrum grants as seen on [Karma](https://gap.karmahq.xyz/arbitrum).")
-    URL_STRING = "https://github.com/OOlajide/arb-grants-chatbot/tree/main"
-    st.markdown(
-        f'<a href="{URL_STRING}" style="display: inline-block; padding: 12px 20px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Action Text on Button</a>',
-        unsafe_allow_html=True
-    )
+    
 # load dataset
 dataset = pd.read_csv("grants_dataset.csv")
 # Instantiate a LLM
