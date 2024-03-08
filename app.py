f69c530ae710581050fad3949107710da629529d
OOlajide <70548782+OOlajide@users.noreply.github.com>
2024-03-08 07:15:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ llm = OpenAI(api_token=st.secrets.api_key)
 df = SmartDataframe(dataset, config={"llm": llm})
 
 with st.form("Question"):
-  question = st.text_input("Question", value="What are the top 5 grantees by amount received, and how much did they receive?", type="default")
+  question = st.text_area("Question", value="What are the top 5 grantees by amount received, and how much did they receive?", type="default")
   submitted = st.form_submit_button("Submit")
   if submitted:
     with st.spinner("Thinking..."):
